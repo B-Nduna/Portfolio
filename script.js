ca9163abdff0f8388088d691e8dc05f7854ad0a4
Bongani Nduna <159546536+B-Nduna@users.noreply.github.com>
2024-06-23 16:19:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ function toggleMenu() {
 
 // Hover effect for project sections
 document.querySelectorAll('.color-container').forEach((project) => {
-  project.addEventListener('mouseenter', () => {
-    document.querySelectorAll('.color-container').forEach((item) => {
-      if (item !== project) {
-        item.classList.add('blur');
-      } else {
-        item.classList.add('focus');
-      }
-    });
-  });
+  if (window.innerWidth > 768) { 
+      project.addEventListener('mouseenter', () => {
+          document.querySelectorAll('.color-container').forEach((item) => {
+              if (item !== project) {
+                  item.classList.add('blur');
+              } else {
+                  item.classList.add('focus');
+              }
+          });
+      });
 
-  project.addEventListener('mouseleave', () => {
-    document.querySelectorAll('.color-container').forEach((item) => {
-      item.classList.remove('blur', 'focus');
-    });
-  });
+      project.addEventListener('mouseleave', () => {
+          document.querySelectorAll('.color-container').forEach((item) => {
+              item.classList.remove('blur', 'focus');
+          });
+      });
+  }
 });
